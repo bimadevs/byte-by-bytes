6f702247fa7f1f75660d7df3a04d483e75b2f164
bimadevs <bimadev06@gmail.com>
2025-04-22 10:54:01 +0700

    Fix: Seo and Link to grub chat whatsapp

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -3,7 +3,7 @@
  */
 
 // Link grup WhatsApp ByteByByte
-export const WHATSAPP_GROUP_LINK = "https://chat.whatsapp.com/KtmCgWg4E26Dc0Wtt1Ao1l";
+export const WHATSAPP_GROUP_LINK = "https://chat.whatsapp.com/KtmCgWg4E26DcOWtt1Ao1I";
 
 // Jumlah anggota komunitas
 export const COMMUNITY_MEMBER_COUNT = 143; 
